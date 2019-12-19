09649405dd3f73b551cc0c3e3b22dd600351e343
catalina torres <catalina.torres.arancibia@gmail.com>
2019-12-19 11:27:31 -0300

    comp home

--- simuladorCreditos/src/app/app.module.ts
+++ simuladorCreditos/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
